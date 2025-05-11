d00103aa3f6f088b59200bbd9aef7bb13c5bafd4
oOccasio <dhzktldh@gmail.com>
2025-05-11 19:13:47 +0900

    test: 더미데이터 삽입 후 테스트

--- src/main/java/umc/spring/domain/Store.java
+++ src/main/java/umc/spring/domain/Store.java
@@ -36,4 +36,14 @@ public class Store extends BaseEntity {
     @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
     private List<Review> reviewList = new ArrayList<>();
 
+    @Override
+    public String toString() {
+        return "Store {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", score=" + score +
+                ", region=" + (region != null ? region.getName() : "N/A") + // region의 이름 출력
+                '}';
+    }
 }
